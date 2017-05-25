e328ea204d4e671810b5c27325aee5b9be734474
abhishek <1607abhishekpatel@gmail.com>
2017-05-26 00:14:32 +0530

    rohit roxx

---
+++ client.java
@@ -0,0 +1,14 @@
+import java.net.*;
+
+class meraclient
+{
+    public static void main(String arg[]) throws Exception
+    {
+      
+     Socket s=new Socket("127.0.0.1",2024);
+     System.out.println("Beta Connection hogaya hai!!!");
+
+     
+      
+    }
+}
